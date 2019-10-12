75024ba5f44c00cd6eef7ce43b117ce0d537b519
mccoylim <56344597+mccoylim@users.noreply.github.com>
2019-10-12 13:40:45 +0800

    Update piano-blochs.py

--- piano-blochs.py
+++ piano-blochs.py
@@ -25,6 +25,32 @@ SCREEN_HEIGHT=650
 X_COLOR = "green"
 Y_COLOR = "orange"
 Z_COLOR = "blue"
+<<<<<<< Updated upstream
+=======
+TAPZONE = np.array([[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]])
+xs,ys,zs = [],[],[]
+hit = 0
+
+def RandomEventGenerator(simulator):
+    global xs
+    global ys
+    global zs
+    rand = QuantumCircuit(6,6)
+    rand.h([0,1,2,3,4,5])
+    rand.measure([0,1,2,3,4,5],[0,1,2,3,4,5])
+    counts = execute(rand,backend=simulator,shots=1).result().get_counts()
+    for key in counts:
+        a = key
+    b = [pos for pos, char in enumerate(a) if char == '1']
+    if len(b) == 0:
+        xs = TAPZONE[:][0]
+        ys = TAPZONE[:][1]
+        zs = TAPZONE[:][2]
+    else:
+        xs = np.array([TAPZONE[i][0] for i in b])
+        ys = np.array([TAPZONE[i][1] for i in b])
+        zs = np.array([TAPZONE[i][2] for i in b])
+>>>>>>> Stashed changes
 
 class Arrow3D(FancyArrowPatch):
 
@@ -56,8 +82,17 @@ time_delay_out = 1000
 time0 = int(pygame.time.get_ticks())
 
 #Song stuff:
+<<<<<<< Updated upstream
 timing = [5000, 7000]
 list = np.array([[0.6,0.8], [-0.8, 0.6]])
+=======
+timing = np.floor(((np.array(range(70)) + 1) * EASY/BPM * 60 + 2) * 1000)
+checklist=np.zeros(len(timing))
+
+combo=0
+combotext=str()
+score = 0
+>>>>>>> Stashed changes
 
 # Getting the wav
 song = pygame.mixer.Sound("testes.wav") #use wav is best apparently
@@ -102,10 +137,41 @@ while running:
                 for key in counts:
                     sv_arr[int(key)] = 1
                     sv_arr[1-int(key)] = 0
+<<<<<<< Updated upstream
                 # Check if statevector coincides with point here
  
         elif event.type == QUIT:
             running = False
+=======
+                hit = 1
+
+        elif event.type == QUIT:
+            running = False
+
+    if hit == 1 and time < timing[0] - 1000:
+        continue
+    elif hit == 0 and time > timing[0] + 1000:
+        combo = 0
+        combotext = "MISS!"
+        np.delete(timing,0)
+    elif hit == 1 and abs(time - timing[0]) < 500 and 1-int(key) in zs:
+        combo += 1
+        combotext = "PERFECT!"
+        score += 1/(1+abs(time-timing[0]))*score_per_note
+        np.delete(timing,0)
+        hit = 0
+    elif hit == 1 and abs(time - timing[0]) < 1000 and 1-int(key) in zs:
+        combo += 1
+        combotext = "GREAT!"
+        score += 1/(1+abs(time-timing[0]))*score_per_note
+        np.delete(timing,0)
+        hit = 0
+    elif hit == 1 and abs(time - timing[0]) < 1000 and 1-int(key) not in zs:
+        combo = 0
+        combotext = "MISS!"
+        np.delete(timing,0)
+        hit = 0
+>>>>>>> Stashed changes
     
     if xcnt > 0:
         circuit.rx(pi / TURN_FRAMES, 0)
@@ -162,6 +228,7 @@ while running:
     ax.add_artist(a)
 
     #add points for the rhythm
+<<<<<<< Updated upstream
     for i in range(len(timing)):
         if timing[i] - time < time_delay and time < timing[i]: #fade in
 #             plot point list[i] on bloch sphere with opacity = 1- (timing[i]-time)/time delay
@@ -185,10 +252,17 @@ while running:
 #             print(str(time)+"No")
         else:
             continue
+=======
+    RandomEventGenerator(simulator)
+#    if timing[0] - time < time_delay and time < timing[0]: #fade in
+#        ax.scatter(xs,ys,zs, s=200, color=(0.5,0,1,1/len(xs)-(timing[0]-time)/time_delay))
+#    elif time - timing[0] < time_delay_out and time > timing[0]: #fade out
+#        ax.scatter(xs,ys,zs, s=200, color=(0.5,0,1,1/len(xs)-(time-timing[0])/time_delay_out))
+#    else:
+#        continue
+>>>>>>> Stashed changes
 
     plt.savefig('bloch.tiff')
-    #     A = plot_bloch_multivector(statevector)
-    #     A.savefig('bloch.png')
     surf = pygame.image.load("bloch.tiff")
     screen.blit(surf,(0,0))
 
