e0e27dff936f50680c85e83ad3d6b4ed1f2aa520
mccoylim <56344597+mccoylim@users.noreply.github.com>
2019-10-12 13:50:41 +0800

    start screen

--- piano-blochs.py
+++ piano-blochs.py
@@ -107,6 +107,19 @@ zcnt = 0
 x_ax_x = y_ax_y = z_ax_z = 1.8
 x_ax_y = x_ax_z = y_ax_x = y_ax_z = z_ax_x = z_ax_y = 0
 
+# Start screen
+black=(0,0,0)
+end_it=False
+while (end_it==False):
+    window.fill(black)
+    myfont=pygame.font.SysFont("Britannic Bold", 40)
+    nlabel=myfont.render("Welcome "+myname+" Start Screen", 1, (255, 0, 0))
+    for event in pygame.event.get():
+        if event.type==MOUSEBUTTONDOWN:
+            end_it=True
+    window.blit(nlabel,(200,200))
+    pygame.display.flip()
+
 while running:
     screen.fill((0, 0, 0))
     
