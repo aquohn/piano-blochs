9a4c602643c34d089cb7ffd042583cbc12e86dc0
John Khoo <john_khoo@u.nus.edu>
2019-10-12 15:42:46 +0800

    Implement game over screen

--- piano-blochs.py
+++ piano-blochs.py
@@ -34,6 +34,7 @@ TAPZONE = [[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]]
 x, y, z = 0, 0, 0
 lock = False
 
+
 class Arrow3D(FancyArrowPatch):
 
     def __init__(self, xs, ys, zs, *args, **kwargs):
@@ -119,214 +120,234 @@ while (end_it==False):
     
 time0 = int(pygame.time.get_ticks())
 pygame.mixer.music.play(0) #i think 0 = play 1 time, 1 is for 2 times, -1 is for infinite
+persistence = 1
 
 while running:
-    screen.fill((0, 0, 0))
-    time = int(pygame.time.get_ticks())-time0
-    
-    # Initialization
-    circuit = QuantumCircuit(1,1) #1 qubit and 1 classical bit
-    sv_arr = np.array([sv_arr[0]+B_FIELD, sv_arr[1]])
-    sv_arr = sv_arr/((abs(sv_arr[0]))**2+(abs(sv_arr[1]))**2)**0.5
-    circuit.initialize(sv_arr, 0)
+    if persistence == 1:
+        screen.fill((0, 0, 0))
+        time = int(pygame.time.get_ticks())-time0
+        
+        # Initialization
+        circuit = QuantumCircuit(1,1) #1 qubit and 1 classical bit
+        sv_arr = np.array([sv_arr[0]+B_FIELD, sv_arr[1]])
+        sv_arr = sv_arr/((abs(sv_arr[0]))**2+(abs(sv_arr[1]))**2)**0.5
+        circuit.initialize(sv_arr, 0)
 
-    circuit.u3(0,0,rot*rotfactor,0)
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if not lock:
-                lock = True
-                if event.key == K_a:
-                    xcnt += TURN_FRAMES
-                elif event.key == K_s:
-                    ycnt += TURN_FRAMES
-                elif event.key == K_d:
-                    zcnt += TURN_FRAMES
-                else:
-                    lock = False
-            
-            if event.key == K_ESCAPE:
+        circuit.u3(0,0,rot*rotfactor,0)
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if not lock:
+                    lock = True
+                    if event.key == K_a:
+                        xcnt += TURN_FRAMES
+                    elif event.key == K_s:
+                        ycnt += TURN_FRAMES
+                    elif event.key == K_d:
+                        zcnt += TURN_FRAMES
+                    else:
+                        lock = False
+                
+                if event.key == K_ESCAPE:
+                    running = False
+                elif event.key == K_f:
+                    persistence = 0
+                elif event.key == K_RETURN:
+                    circuit.measure([0],[0])
+                    backend = Aer.get_backend('qasm_simulator')
+                    result = execute(circuit,backend=backend,shots = 1).result()
+                    counts = result.get_counts()
+                    for key in counts:
+                        sv_arr[int(key)] = 1
+                        sv_arr[1-int(key)] = 0
+                    hit = 1
+            elif event.type == QUIT:
                 running = False
-            elif event.key == K_RETURN:
-                circuit.measure([0],[0])
-                backend = Aer.get_backend('qasm_simulator')
-                result = execute(circuit,backend=backend,shots = 1).result()
-                counts = result.get_counts()
-                for key in counts:
-                    sv_arr[int(key)] = 1
-                    sv_arr[1-int(key)] = 0
-                hit = 1
-        elif event.type == QUIT:
-            running = False
-            
-    valid_hit = (sv_arr[0] == 1 and z == 1) or (sv_arr[0] == 0 and z == -1)
+                
+        valid_hit = (sv_arr[0] == 1 and z == 1) or (sv_arr[0] == 0 and z == -1)
 
-    if hit == 1 and time < note_time_arr[0] - 1000:
-        continue
-    elif hit == 0 and time > note_time_arr[0] + 1000:
-        combo = 0
-        combotext = "MISS!"
-        np.delete(note_time_arr,0)
-    elif hit == 1 and abs(time - note_time_arr[0]) < 500 and valid_hit:
-        combo += 1
-        combotext = "PERFECT!"
-        score += 1/(1+abs(time-note_time_arr[0]))*SCORE_PER_NOTE
-        np.delete(note_time_arr,0)
-        hit = 0
-    elif hit == 1 and abs(time - note_time_arr[0]) < 1000:
-        if valid_hit:
-            combo += 1
-            combotext = "GREAT!"
-            score += 1/(1+abs(time-note_time_arr[0]))*SCORE_PER_NOTE
-        else:
+        if hit == 1 and time < note_time_arr[0] - 1000:
+            continue
+        elif hit == 0 and time > note_time_arr[0] + 1000:
             combo = 0
             combotext = "MISS!"
-        hit = 0
-        np.delete(note_time_arr,0)
-    
-    # Rotation
-    if xcnt > 0:
-        assert(lock == True)
-        # axis rotation
-        x_ax_arr = np.matmul(rotX, np.array([x_ax_x, x_ax_y, x_ax_z]))
-        y_ax_arr = np.matmul(rotX, np.array([y_ax_x, y_ax_y, y_ax_z]))
-        z_ax_arr = np.matmul(rotX, np.array([z_ax_x, z_ax_y, z_ax_z]))
-
-        x_ax_y = x_ax_arr[1]
-        x_ax_z = x_ax_arr[2]
-        y_ax_y = y_ax_arr[1]
-        y_ax_z = y_ax_arr[2]
-        z_ax_y = z_ax_arr[1]
-        z_ax_z = z_ax_arr[2]
+            np.delete(note_time_arr,0)
+        elif hit == 1 and abs(time - note_time_arr[0]) < 500 and valid_hit:
+            combo += 1
+            combotext = "PERFECT!"
+            score += 1/(1+abs(time-note_time_arr[0]))*SCORE_PER_NOTE
+            np.delete(note_time_arr,0)
+            hit = 0
+        elif hit == 1 and abs(time - note_time_arr[0]) < 1000:
+            if valid_hit:
+                combo += 1
+                combotext = "GREAT!"
+                score += 1/(1+abs(time-note_time_arr[0]))*SCORE_PER_NOTE
+            else:
+                combo = 0
+                combotext = "MISS!"
+            hit = 0
+            np.delete(note_time_arr,0)
         
-        # state vector rotation
-        circuit.rx(theta, 0)
+        # Rotation
+        if xcnt > 0:
+            assert(lock == True)
+            # axis rotation
+            x_ax_arr = np.matmul(rotX, np.array([x_ax_x, x_ax_y, x_ax_z]))
+            y_ax_arr = np.matmul(rotX, np.array([y_ax_x, y_ax_y, y_ax_z]))
+            z_ax_arr = np.matmul(rotX, np.array([z_ax_x, z_ax_y, z_ax_z]))
 
-        xcnt -= 1 
+            x_ax_y = x_ax_arr[1]
+            x_ax_z = x_ax_arr[2]
+            y_ax_y = y_ax_arr[1]
+            y_ax_z = y_ax_arr[2]
+            z_ax_y = z_ax_arr[1]
+            z_ax_z = z_ax_arr[2]
+            
+            # state vector rotation
+            circuit.rx(theta, 0)
 
-    if ycnt > 0:
-        assert(lock == True)
-        # axis rotation
-        x_ax_arr = np.matmul(rotY, np.array([x_ax_x, x_ax_y, x_ax_z]))
-        y_ax_arr = np.matmul(rotY, np.array([y_ax_x, y_ax_y, y_ax_z]))
-        z_ax_arr = np.matmul(rotY, np.array([z_ax_x, z_ax_y, z_ax_z]))
+            xcnt -= 1 
 
-        x_ax_x = x_ax_arr[0]
-        x_ax_z = x_ax_arr[2]
-        y_ax_x = y_ax_arr[0]
-        y_ax_z = y_ax_arr[2]
-        z_ax_x = z_ax_arr[0]
-        z_ax_z = z_ax_arr[2]
+        if ycnt > 0:
+            assert(lock == True)
+            # axis rotation
+            x_ax_arr = np.matmul(rotY, np.array([x_ax_x, x_ax_y, x_ax_z]))
+            y_ax_arr = np.matmul(rotY, np.array([y_ax_x, y_ax_y, y_ax_z]))
+            z_ax_arr = np.matmul(rotY, np.array([z_ax_x, z_ax_y, z_ax_z]))
 
-        # state vector rotation
-        circuit.ry(theta, 0)
+            x_ax_x = x_ax_arr[0]
+            x_ax_z = x_ax_arr[2]
+            y_ax_x = y_ax_arr[0]
+            y_ax_z = y_ax_arr[2]
+            z_ax_x = z_ax_arr[0]
+            z_ax_z = z_ax_arr[2]
 
-        ycnt -= 1
+            # state vector rotation
+            circuit.ry(theta, 0)
 
-    if zcnt > 0:
-        assert(lock == True)
-        # axis rotation
-        x_ax_arr = np.matmul(rotZ, np.array([x_ax_x, x_ax_y, x_ax_z]))
-        y_ax_arr = np.matmul(rotZ, np.array([y_ax_x, y_ax_y, y_ax_z]))
-        z_ax_arr = np.matmul(rotZ, np.array([z_ax_x, z_ax_y, z_ax_z]))
+            ycnt -= 1
 
-        x_ax_x = x_ax_arr[0]
-        x_ax_y = x_ax_arr[1]
-        y_ax_x = y_ax_arr[0]
-        y_ax_y = y_ax_arr[1]
-        z_ax_x = z_ax_arr[0]
-        z_ax_y = z_ax_arr[1]
+        if zcnt > 0:
+            assert(lock == True)
+            # axis rotation
+            x_ax_arr = np.matmul(rotZ, np.array([x_ax_x, x_ax_y, x_ax_z]))
+            y_ax_arr = np.matmul(rotZ, np.array([y_ax_x, y_ax_y, y_ax_z]))
+            z_ax_arr = np.matmul(rotZ, np.array([z_ax_x, z_ax_y, z_ax_z]))
 
-        # state vector rotation
-        circuit.rz(theta, 0)
+            x_ax_x = x_ax_arr[0]
+            x_ax_y = x_ax_arr[1]
+            y_ax_x = y_ax_arr[0]
+            y_ax_y = y_ax_arr[1]
+            z_ax_x = z_ax_arr[0]
+            z_ax_y = z_ax_arr[1]
 
-        zcnt -= 1
-    
-    if (xcnt == ycnt == zcnt == 0):
-        lock = False
-    
-    fig = plt.figure(figsize=(10,10))
-    ax = Axes3D(fig)
-    ax._axis3don = False
-    ax.set_xlim3d(-1.3, 1.3)
-    ax.set_ylim3d(-1.3, 1.3)
-    ax.set_zlim3d(-1.3, 1.3)
-    
-    # z-axis
-    ax.plot([-z_ax_x, z_ax_x], [-z_ax_y, z_ax_y], [-z_ax_z, z_ax_z], color=Z_COLOR)
-    ax.text(z_ax_x, z_ax_y, z_ax_z, " + z", color=Z_COLOR)
-    ax.text(-z_ax_x, -z_ax_y, -z_ax_z, " - z", color=Z_COLOR)
-    
-    # y-axis
-    ax.plot([-y_ax_x, y_ax_x], [-y_ax_y, y_ax_y], [-y_ax_z, y_ax_z], color=Y_COLOR)
-    ax.text(y_ax_x, y_ax_y, y_ax_z, " + y", color=Y_COLOR)
-    ax.text(-y_ax_x, -y_ax_y, -y_ax_z, " - y", color=Y_COLOR)
-    
-    # x-axis
-    ax.plot([-x_ax_x, x_ax_x], [-x_ax_y, x_ax_y], [-x_ax_z, x_ax_z], color=X_COLOR)
-    ax.text(x_ax_x, x_ax_y, x_ax_z, " + x", color=X_COLOR)
-    ax.text(-x_ax_x, -x_ax_y, -x_ax_z, " - x", color=X_COLOR)
+            # state vector rotation
+            circuit.rz(theta, 0)
 
-    # draw sphere
-    u, v = np.mgrid[0:2*pi:50j, 0:pi:25j]
-    x = np.cos(u)*np.sin(v)
-    y = np.sin(u)*np.sin(v)
-    z = np.cos(v)
-    ax.plot_wireframe(x, y, z, color=(0,0,0,0.2))
-
-    result = execute(circuit, backend = simulator).result()
-    sv_arr = result.get_statevector()
-    z1 = [0,-np.real(1-2*np.conj(sv_arr[0])*sv_arr[0])]
-    rotfactor = abs(z1[1]) + 0.05 # smooth precession
-    x1 = [0,2*(np.real(sv_arr[0])*np.real(sv_arr[1])+np.imag(sv_arr[0])*np.imag(sv_arr[1]))]
-    y1 = [0,2*(np.imag(sv_arr[0])*np.real(sv_arr[1])+np.real(sv_arr[0])*np.imag(sv_arr[1]))]
-    n = (x1[1]**2+y1[1]**2+z1[1]**2)**0.5
-    a = Arrow3D(x1/n, y1/n, z1/n, mutation_scale=20,
-            lw=1, arrowstyle="-|>", color='r')
-    ax.add_artist(a)
-
-    #add points for the rhythm
-    for i in range(len(note_time_arr)):
-        xarr = [x_ax_x, y_ax_x, z_ax_x]
-        yarr = [x_ax_y, y_ax_y, z_ax_y]
-        zarr = [x_ax_z, y_ax_z, z_ax_z]
+            zcnt -= 1
         
-        idx = point_idxs[i] - 3
-        if idx < 0:
-            idx = (-1 * idx) - 1
-            x = -xarr[idx] / AXIS_SCALE
-            y = -yarr[idx] / AXIS_SCALE
-            z = -zarr[idx] / AXIS_SCALE
-        else:
-            x = xarr[idx] / AXIS_SCALE
-            y = yarr[idx] / AXIS_SCALE
-            z = zarr[idx] / AXIS_SCALE
+        if (xcnt == ycnt == zcnt == 0):
+            lock = False
         
-        if note_time_arr[i] - time < time_delay and time < note_time_arr[i]: #fade in
+        fig = plt.figure(figsize=(10,10))
+        ax = Axes3D(fig)
+        ax._axis3don = False
+        ax.set_xlim3d(-1.3, 1.3)
+        ax.set_ylim3d(-1.3, 1.3)
+        ax.set_zlim3d(-1.3, 1.3)
+        
+        # z-axis
+        ax.plot([-z_ax_x, z_ax_x], [-z_ax_y, z_ax_y], [-z_ax_z, z_ax_z], color=Z_COLOR)
+        ax.text(z_ax_x, z_ax_y, z_ax_z, " + z", color=Z_COLOR)
+        ax.text(-z_ax_x, -z_ax_y, -z_ax_z, " - z", color=Z_COLOR)
+        
+        # y-axis
+        ax.plot([-y_ax_x, y_ax_x], [-y_ax_y, y_ax_y], [-y_ax_z, y_ax_z], color=Y_COLOR)
+        ax.text(y_ax_x, y_ax_y, y_ax_z, " + y", color=Y_COLOR)
+        ax.text(-y_ax_x, -y_ax_y, -y_ax_z, " - y", color=Y_COLOR)
+        
+        # x-axis
+        ax.plot([-x_ax_x, x_ax_x], [-x_ax_y, x_ax_y], [-x_ax_z, x_ax_z], color=X_COLOR)
+        ax.text(x_ax_x, x_ax_y, x_ax_z, " + x", color=X_COLOR)
+        ax.text(-x_ax_x, -x_ax_y, -x_ax_z, " - x", color=X_COLOR)
+
+        # draw sphere
+        u, v = np.mgrid[0:2*pi:50j, 0:pi:25j]
+        x = np.cos(u)*np.sin(v)
+        y = np.sin(u)*np.sin(v)
+        z = np.cos(v)
+        ax.plot_wireframe(x, y, z, color=(0,0,0,0.2))
+
+        result = execute(circuit, backend = simulator).result()
+        sv_arr = result.get_statevector()
+        z1 = [0,-np.real(1-2*np.conj(sv_arr[0])*sv_arr[0])]
+        rotfactor = abs(z1[1]) + 0.05 # smooth precession
+        x1 = [0,2*(np.real(sv_arr[0])*np.real(sv_arr[1])+np.imag(sv_arr[0])*np.imag(sv_arr[1]))]
+        y1 = [0,2*(np.imag(sv_arr[0])*np.real(sv_arr[1])+np.real(sv_arr[0])*np.imag(sv_arr[1]))]
+        n = (x1[1]**2+y1[1]**2+z1[1]**2)**0.5
+        a = Arrow3D(x1/n, y1/n, z1/n, mutation_scale=20,
+                lw=1, arrowstyle="-|>", color='r')
+        ax.add_artist(a)
+
+        #add points for the rhythm
+        for i in range(len(note_time_arr)):
+            xarr = [x_ax_x, y_ax_x, z_ax_x]
+            yarr = [x_ax_y, y_ax_y, z_ax_y]
+            zarr = [x_ax_z, y_ax_z, z_ax_z]
+            
+            idx = point_idxs[i] - 3
+            if idx < 0:
+                idx = (-1 * idx) - 1
+                x = -xarr[idx] / AXIS_SCALE
+                y = -yarr[idx] / AXIS_SCALE
+                z = -zarr[idx] / AXIS_SCALE
+            else:
+                x = xarr[idx] / AXIS_SCALE
+                y = yarr[idx] / AXIS_SCALE
+                z = zarr[idx] / AXIS_SCALE
+            
+            if note_time_arr[i] - time < time_delay and time < note_time_arr[i]: #fade in
 #             plot point list[i] on bloch sphere with opacity = 1- (note_time_arr[i]-time)/time delay
-            ax.scatter(x,y,z, s=200, color=(0.5,0,1,1-(note_time_arr[i]-time)/time_delay)) 
+                ax.scatter(x,y,z, s=200, color=(0.5,0,1,1-(note_time_arr[i]-time)/time_delay)) 
 
-        elif time - note_time_arr[i] < time_delay_out and time > note_time_arr[i]: #fade out
+            elif time - note_time_arr[i] < time_delay_out and time > note_time_arr[i]: #fade out
 #             plot point list[i] on bloch sphere with opacity = 1- (time. - note_time_arr[i])/time delay out
-            ax.scatter(x,y,z, s=200, color=(0.5,0,1,1- (time - note_time_arr[i])/time_delay_out)) 
+                ax.scatter(x,y,z, s=200, color=(0.5,0,1,1- (time - note_time_arr[i])/time_delay_out)) 
 #             print(str(time)+"No")
-        else:
-            continue
+            else:
+                continue
 
-    plt.savefig('bloch.tiff')
-    surf = pygame.image.load("bloch.tiff")
-    screen.blit(surf,(0,0))
+        plt.savefig('bloch.tiff')
+        surf = pygame.image.load("bloch.tiff")
+        screen.blit(surf,(0,0))
 
-    font = pygame.font.SysFont('comicsans', 30, True)
-    timetext = font.render("Time: " + str(time), 1, (0,0,0)) 
-    combohaha = font.render("Combo: " + str(combo), 1, (0,0,0))
-    combotext2 = font.render(str(combotext), 1, (0,0,0))
-    screen.blit(timetext, (0, 0))
-    screen.blit(combohaha, (200, 0))
-    screen.blit(combotext2, (400, 0))
+        font = pygame.font.SysFont('comicsans', 30, True)
+        timetext = font.render("Time: " + str(time), 1, (0,0,0)) 
+        combohaha = font.render("Combo: " + str(combo), 1, (0,0,0))
+        combotext2 = font.render(str(combotext), 1, (0,0,0))
+        screen.blit(timetext, (0, 0))
+        screen.blit(combohaha, (200, 0))
+        screen.blit(combotext2, (400, 0))
 
-    pygame.display.flip()
-    
-    clock.tick(30)
-    plt.close(fig)
+        pygame.display.flip()
+        
+        clock.tick(30)
+        plt.close(fig)
+    else:
+        screen.fill(black)
+        myfont=pygame.font.SysFont("Britannic Bold", 40)
+        nlabel=myfont.render("Gameover", 1, (255, 0, 0))
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key==K_ESCAPE:
+                    running = False
+                if event.key==K_f:
+                    persistence = 1
+                    time0=int(pygame.time.get_ticks())
+                    pygame.mixer.music.play(0)
+            elif event.type == QUIT:
+                running = False
+        screen.blit(nlabel,(200,200))
+        pygame.display.flip()
 
 pygame.quit()
